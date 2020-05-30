88c9fd2d8688fd19ff921366a92c280db3b00fed
Zied Nouira <zied.nouira@gmail.com>
2020-05-29 23:27:00 -0400

    wip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,43 +10,44 @@ import {BoardComponent as contreBoard} from './contre/Client';
 import 'antd/dist/antd.css';
 import {SocketIO} from 'boardgame.io/multiplayer';
 
-// const App: React.FunctionComponent = () => {
-//   if (!process.env.REACT_APP_API_BASE_URL) {
-//     throw new Error('REACT_APP_API_BASE_URL env var must be set');
-//   }
-//
-//   return (
-//     <Router>
-//       <Switch>
-//         <Route path="/">
-//           <Lobby
-//             gameServer={process.env.REACT_APP_API_BASE_URL}
-//             lobbyServer={process.env.REACT_APP_API_BASE_URL}
-//             gameComponents={[
-//               { game: coincheGame, board: coincheBoard },
-//               { game: contreGame, board: contreBoard },
-//             ]}
-//           />
-//         </Route>
-//       </Switch>
-//     </Router>
-//   );
-// };
+const App: React.FunctionComponent = () => {
+  const PORT = process.env.BGIO_PORT || '8000';
+  const server = `http://localhost:${PORT}`;
+  // if (!process.env.REACT_APP_API_BASE_URL) {
+  //   throw new Error('REACT_APP_API_BASE_URL env var must be set');
+  // }
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Lobby
+            gameServer={server}
+            lobbyServer={server}
+            gameComponents={[
+              { game: contreGame, board: contreBoard },
+            ]}
+          />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 // const App = Client({ game: coincheGame, board: coincheBoard, numPlayers: 4 });
 // const App = Client({ game: contreGame, board: contreBoard, numPlayers: 4 });
 
-const ContreClient = Client({
-  game: contreGame,
-  board: contreBoard,
-  multiplayer: SocketIO({ server: 'http://localhost:8000/' }),
-});
-
-const App =  () => (
-  <div>
-    <ContreClient playerID="0" />
-    <ContreClient playerID="1" />
-    <ContreClient playerID="2" />
-    <ContreClient playerID="3" />
-  </div>
-);
+// const ContreClient = Client({
+//   game: contreGame,
+//   board: contreBoard,
+//   multiplayer: SocketIO({ server: 'http://localhost:8000/' })
+// });
+//
+// const App =  () => (
+//   <div>
+//     <ContreClient playerID="0" />
+//     <ContreClient playerID="1" />
+//     <ContreClient playerID="2" />
+//     <ContreClient playerID="3" />
+//   </div>
+// );
 export default App;
